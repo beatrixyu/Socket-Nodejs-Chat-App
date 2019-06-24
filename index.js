9cb059ac1bc95bb4741754024aa9dc474c864e8e
beatrixyu <beatrixyu@126.com>
2019-06-24 15:06:34 +0200

    first upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
+app.get("/chat-group-app/", function(req, res) {
   // res.send("Hello world!");
   res.sendFile(__dirname + "/index.html");
 });
